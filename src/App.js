d6b2b8f46e9e785395eb2b4f7deac8f4bed3d544
Sukeerth <ssvegaraju@gmail.com>
2022-06-07 14:02:35 -0700

    add initial state structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Welcome from './Components/Welcome/Welcome';
+
+const CurrentScene = {
+  Welcome: 'Welcome',
+  AboutMe: 'AboutMe',
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [currentScene, setCurrentScene] = useState(CurrentScene.Welcome);
+  switch(currentScene) {
+    case CurrentScene.Welcome:
+      return <Welcome></Welcome>;
+    case CurrentScene.AboutMe:
+      break;
+    default:
+      break;
+  }
+  return <div>You shouldn't be seeing this, something went wrong!</div>
 }
 
 export default App;
---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function Welcome() {
+
+
+    return (
+        <div>
+            This is the welcome page!
+        </div>
+    )
+}
+
+export default Welcome;
